35e7e6485678b5715c943960671be374ea06a38e
Satrix <Satrix@Satrix-PC>
2011-05-22 11:53:56 +0200

    Commit

---
+++ src/main/java/com/mycompany/test/AddValues.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author Satrix
+ */
+public class AddValues {
+    private int a;
+    private int b;
+
+    public AddValues(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
